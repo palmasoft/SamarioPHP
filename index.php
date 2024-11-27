e2cfb30d2f4359dfbd695da6386a2c259af64749
ingllinasramirez <ing.llinasramirez@gmail.com>
2024-11-26 21:54:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,2 @@
 <?php
-
-echo phpinfo(); 
-
-echo "<pre>";
-echo "********************************************\n";
-echo "*                                          *\n";
-echo "*      NEW APP UNDER CONSTRUCTION!          *\n";
-echo "*                                          *\n";
-echo "********************************************\n";
-echo "*    _______    ___________    _____       *\n";
-echo "*   |       |  |           |  |     |      *\n";
-echo "*   |  [ ]  |  |           |  | [ ] |      *\n";
-echo "*   |       |  |___________|  |     |      *\n";
-echo "*   |   __  |  |   [ ]     |  |_____|      *\n";
-echo "*                                          *\n";
-echo "*      Please check back soon.             *\n";
-echo "*                                          *\n";
-echo "********************************************\n";
-echo "</pre>";
-?>
+phpinfo();

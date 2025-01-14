7a8cd0f94c6dff97b5ba70b7d97142cec0008f8b
ingllinasramirez <ing.llinasramirez@gmail.com>
2025-01-14 16:59:08 -0500

    Create ejemplos.php

---
+++ ejemplos.php
@@ -0,0 +1,46 @@
+<table border="1" style="border-collapse: collapse; width: 100%;">
+    <thead>
+        <tr style="background-color: #f0f0f0; text-align: left;">
+            <th style="padding: 8px;">Categoría</th>
+            <th style="padding: 8px;">Nombre</th>
+            <th style="padding: 8px;">Enlace</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Persona Natural</td>
+            <td>Matrícula Mercantil y Renovación</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/815/26e2ea4322d70b4469cde9c8f375f5c6.pdf" target="_blank">https://s3.pagegear.co/425/documentos/815/26e2ea4322d70b4469cde9c8f375f5c6.pdf</a></td>
+        </tr>
+        <tr>
+            <td>Persona Natural</td>
+            <td>Cancelación de Matrícula Mercantil</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/816/d4e1cbf312a2a18bb4ed3bba55299856.pdf" target="_blank">https://s3.pagegear.co/425/documentos/816/d4e1cbf312a2a18bb4ed3bba55299856.pdf</a></td>
+        </tr>
+        <tr>
+            <td>Persona Jurídica</td>
+            <td>Constitución de Sociedades</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/819/c933bbbd31dfcc05d7f3521541fa1be1.pdf" target="_blank">https://s3.pagegear.co/425/documentos/819/c933bbbd31dfcc05d7f3521541fa1be1.pdf</a></td>
+        </tr>
+        <tr>
+            <td>Persona Jurídica</td>
+            <td>Empresa Unipersonal</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/820/ca533d747af63e610a986a1410e38639.pdf" target="_blank">https://s3.pagegear.co/425/documentos/820/ca533d747af63e610a986a1410e38639.pdf</a></td>
+        </tr>
+        <tr>
+            <td>Persona Jurídica</td>
+            <td>Sociedad por Acciones Simplificada</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/821/65ea3f8345bd899212e38c55a5ea316d.pdf" target="_blank">https://s3.pagegear.co/425/documentos/821/65ea3f8345bd899212e38c55a5ea316d.pdf</a></td>
+        </tr>
+        <tr>
+            <td>Persona Jurídica</td>
+            <td>Sociedades de Beneficio de Interés Colectivo</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/822/25b23e25d485b495147cc804f04dd7cd.pdf" target="_blank">https://s3.pagegear.co/425/documentos/822/25b23e25d485b495147cc804f04dd7cd.pdf</a></td>
+        </tr>
+        <tr>
+            <td>Todos</td>
+            <td>Registro Nacional de Turismo RNT</td>
+            <td><a href="https://s3.pagegear.co/425/documentos/892/2c7e5680e694e1e21cb1bbca4d56489f.pdf" target="_blank">https://s3.pagegear.co/425/documentos/892/2c7e5680e694e1e21cb1bbca4d56489f.pdf</a></td>
+        </tr>
+    </tbody>
+</table>
